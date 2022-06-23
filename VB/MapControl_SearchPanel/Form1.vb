c540db4fc3e70d3112b2d19ee7c0a57cecda091d
DevExpressExampleBot <fakeEmail>
2022-06-23 11:59:16 +0400

    Source auto update [skip ci]

--- VB/MapControl_SearchPanel/Form1.vb
+++ VB/MapControl_SearchPanel/Form1.vb
@@ -1,17 +1,19 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace MapControl_SearchPanel
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
-        Private ReadOnly Property SearchLayer() As InformationLayer
+        Private ReadOnly Property SearchLayer As InformationLayer
             Get
                 Return CType(mapControl1.Layers("SearchLayer"), InformationLayer)
             End Get
         End Property
-        Private ReadOnly Property SearchProvider() As BingSearchDataProvider
+
+        Private ReadOnly Property SearchProvider As BingSearchDataProvider
             Get
                 Return CType(SearchLayer.DataProvider, BingSearchDataProvider)
             End Get
@@ -20,13 +22,15 @@ Namespace MapControl_SearchPanel
         Public Sub New()
             InitializeComponent()
             AddHandler SearchLayer.DataRequestCompleted, AddressOf SearchLayer_DataRequestCompleted
+            ' 
+            mapControl1.Layers.Add(New ImageLayer() With {.DataProvider = New BingMapDataProvider() With {.BingKey = "YOUR BING KEY", .Kind = BingMapKind.Area}})
         End Sub
 
         Private Sub SearchLayer_DataRequestCompleted(ByVal sender As Object, ByVal e As RequestCompletedEventArgs)
             mapControl1.ZoomToFitLayerItems()
         End Sub
 
-        Private Sub bSearch_Click(ByVal sender As Object, ByVal e As EventArgs) Handles bSearch.Click
+        Private Sub bSearch_Click(ByVal sender As Object, ByVal e As EventArgs)
             SearchProvider.Search(teKeyword.Text)
         End Sub
     End Class
